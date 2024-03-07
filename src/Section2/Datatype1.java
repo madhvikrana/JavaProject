9352b632235ca7d1d2abdacf649cdfcafed2fabd
framework <madhvikrana@gmail.com>
2024-03-07 15:24:12 +0545

    1st commit

---
+++ src/Section2/Datatype1.java
@@ -0,0 +1,12 @@
+package Section2;
+import java.lang.*;
+
+//Finding minimum and maximum value in a datatype.
+public class Datatype1 {
+    public static void main(String[] args) {
+        System.out.println("Int Min:" + Integer.MIN_VALUE);
+        System.out.println("Int Max:" + Integer.MAX_VALUE);
+        System.out.println("Int Bytes" + Integer.BYTES);
+    }
+    
+}
