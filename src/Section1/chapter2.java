0ee1a1fff8dfa194f92409ae543ac42800d66b60
Madhvi Rana <madhvikrana@gmail.com>
2024-06-24 09:56:37 +0545

    commit98

--- src/Section1/chapter2.java
+++ src/Section1/chapter2.java
@@ -3,10 +3,19 @@ import java.util.*;
 
 public class chapter2
 
+//keyboard
+
 {
     public static void main(String[] args) 
     {
-        String name;
-        
+        //program for adding two numbers
+        Scanner s=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter 2 numbers");
+        a=s.nextInt();
+        b=s.nextInt();
+        c=a+b;
+        System.out.println("sum is" + c);
+
     }
 }
