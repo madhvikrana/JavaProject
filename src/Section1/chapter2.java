b2ed61414ef343446906fc9ae982ab2f05c1346f
Madhvi Rana <madhvikrana@gmail.com>
2024-06-26 13:19:54 +0545

    commit96

--- src/Section1/chapter2.java
+++ src/Section1/chapter2.java
@@ -1,21 +1,32 @@
 package Section1;
+
 import java.util.*;
 
 public class chapter2
 
-//keyboard
+// keyboard
 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
+    // {
+    // //program for adding two numbers
+    // Scanner s=new Scanner(System.in);
+    // int a,b,c;
+    // System.out.println("Enter 2 numbers");
+    // a=s.nextInt();
+    // b=s.nextInt();
+    // c=a+b;
+    // System.out.println("Sum is " + c);
+
+    // }
+
     {
-        //program for adding two numbers
-        Scanner s=new Scanner(System.in);
-        int a,b,c;
-        System.out.println("Enter 2 numbers");
-        a=s.nextInt();
-        b=s.nextInt();
-        c=a+b;
-        System.out.println("Sum is " + c);
+        // program for knowing name
+        Scanner s = new Scanner(System.in);
+        String name;
+        System.out.println("May I know your name?");
+        name = s.nextLine();
+        System.out.println("Welcome Mr/Miss " + name);
 
     }
 }
