7d34c72e3adbc613bbada54c890ff6914518bbed
Madhvi Rana <madhvikrana@gmail.com>
2024-07-01 23:04:32 +0545

    commit96

--- src/Section1/chapter2.java
+++ src/Section1/chapter2.java
@@ -8,25 +8,25 @@ public class chapter2
 
 {
     public static void main(String[] args)
-    // {
-    // //program for adding two numbers
-    // Scanner s=new Scanner(System.in);
-    // int a,b,c;
-    // System.out.println("Enter 2 numbers");
-    // a=s.nextInt();
-    // b=s.nextInt();
-    // c=a+b;
-    // System.out.println("Sum is " + c);
-
-    // }
-
     {
-        // program for knowing name
-        Scanner s = new Scanner(System.in);
-        String name;
-        System.out.println("May I know your name?");
-        name = s.nextLine();
-        System.out.println("Welcome Mr/Miss " + name);
+    //program for adding two numbers
+    Scanner s=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("Enter 2 numbers");
+    a=s.nextInt();
+    b=s.nextInt();
+    c=a+b;
+    System.out.println("Sum is " + c);
 
     }
+
+    // {
+    //     // program for knowing name
+    //     Scanner s = new Scanner(System.in);
+    //     String name;
+    //     System.out.println("May I know your name?");
+    //     name = s.nextLine();
+    //     System.out.println("Welcome Mr/Miss " + name);
+
+    // }
 }
