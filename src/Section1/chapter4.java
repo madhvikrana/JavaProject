0d418f9fffd3bdadbc6b66b44e2564c0e0f5400c
Madhvi Rana <madhvikrana@gmail.com>
2024-07-15 16:34:16 +0545

    commit95

---
+++ src/Section1/chapter4.java
@@ -0,0 +1,12 @@
+package Section1;
+
+import java.util.Scanner;
+
+public class chapter4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float x=sc.nextFloat();
+        System.out.println(x);
+    }
+    
+}
